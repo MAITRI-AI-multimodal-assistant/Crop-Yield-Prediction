cd33c5446be914b6a9d3a1f76c4e1a11a2e1ae52
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-03-30 00:26:32 +0530

    rr

--- Crop-Yield-Prediction-main/backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/send", sendContact);
-
-export default router;
